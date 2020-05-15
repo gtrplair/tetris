998b6a139899d1363f5a23ce148cc2a3d17eb183
gtrplair <fuzz@debian>
2020-05-16 00:12:42 +0200

    Fixed squaresize

--- display.cpp
+++ display.cpp
@@ -1,8 +1,10 @@
-#include <iostream>
-#include <stdio.h>
 #include <GL/glut.h>
-#include <stdlib.h>
 #include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <iostream>
+
 #include "proj.cpp"
 
 #define WIDTHWINDOW 700;
@@ -14,7 +16,8 @@ using namespace std;
 
 int choosingpiece;
 float deplacex, deplacey, xx, yy;
-float move_unit = 0.18f;
+float xmove_unit;
+float ymove_unit;
 
 int shape;
 int width;
@@ -23,150 +26,32 @@ float xborder = 0.2;
 float yborder = 0.2;
 float x0board = -1 + xborder;
 float y0board = 1 - yborder;
-float squaresize = 0.08;
+float xsquaresize, ysquaresize;
 
-void display_plateau(int shape, int width)
-{
-
+void display_plateau(int shape, int width) {
   int row, c;
 
   float x1, x2, y1, y2;
   width /= 2;
   width += 1;
 
+  xsquaresize = 2 * (1-xborder) / width;
+  ysquaresize = 2 * (1-yborder) / width;
+
   x1 = -1 + xborder;
   y1 = 1 - yborder;
 
-  switch (shape)
-  {
-  case 1:
-  {
+  switch (shape) {
+    case 1: {
+      // Triangle
 
-    // Triangle
-
-    for (row = 1; row <= width; row++)
-    {
-
-      for (c = 0; c < width - row; c++)
-      {
-        x1 += squaresize;
-      }
-      for (c = 1; c <= 2 * row - 1; c++)
-      {
-
-        x2 = x1 + squaresize;
-        y2 = y1 - squaresize;
-
-        glColor3f(0.90, 0.22, 0);
-
-        glBegin(GL_QUADS);
-        glVertex2f(x2, y1);
-        glVertex2f(x1, y1);
-        glVertex2f(x1, y2);
-        glVertex2f(x2, y2);
-        glEnd();
-
-        x1 += squaresize;
-      }
-      x1 = -1 + xborder;
-
-      y1 -= squaresize;
-
-      cout << "\n";
-    }
-    break;
-  }
-// this is a test
-
-  case 2:
-  {
-
-    // Diamond
-
-    for (row = 1; row <= width; row++)
-    {
-
-      for (c = 0; c < width - row; c++)
-      {
-        x1 += squaresize;
-      }
-      for (c = 1; c <= 2 * row - 1; c++)
-      {
-
-        x2 = x1 + squaresize;
-        y2 = y1 - squaresize;
-
-        glColor3f(0.90, 0.22, 0);
-
-        glBegin(GL_QUADS);
-        glVertex2f(x2, y1);
-        glVertex2f(x1, y1);
-        glVertex2f(x1, y2);
-        glVertex2f(x2, y2);
-        glEnd();
-
-        x1 += squaresize;
-      }
-      x1 = -1 + xborder;
-
-      y1 -= squaresize;
-
-      cout << "\n";
-    }
-
-    for (row = width; row > 1; row--)
-    {
-
-      for (c = 0; c < width - row + 1; c++)
-      {
-        x1 += squaresize;
-      }
-      for (c = 1; c <= 2 * row - 3; c++)
-      {
-        x2 = x1 + squaresize;
-        y2 = y1 - squaresize;
-
-        glColor3f(0.90, 0.22, 0);
-
-        glBegin(GL_QUADS);
-        glVertex2f(x2, y1);
-        glVertex2f(x1, y1);
-        glVertex2f(x1, y2);
-        glVertex2f(x2, y2);
-        glEnd();
-
-        x1 += squaresize;
-      }
-      x1 = -1 + xborder;
-
-      y1 -= squaresize;
-    }
-    break;
-  }
-
-  case 3:
-  {
-
-    int n = 2 * width + 1;
-
-    int i, j, x, y;
-
-    for (i = 0; i < n; i++)
-    {
-      for (j = 0; j < n; j++)
-      {
-
-        x = i - width;
-        y = j - width;
-
-        if (x * x + y * y <= width * width + 1)
-        {
-
-          x1 += i * squaresize;
-          y1 -= j * squaresize;
-
-          x2 = x1 + squaresize;
-          y2 = y1 - squaresize;
+      for (row = 1; row <= width; row++) {
+        for (c = 0; c < width - row; c++) {
+          x1 += xsquaresize;
+        }
+        for (c = 1; c <= 2 * row - 1; c++) {
+          x2 = x1 + xsquaresize;
+          y2 = y1 - ysquaresize;
 
           glColor3f(0.90, 0.22, 0);
 
@@ -176,21 +61,113 @@ void display_plateau(int shape, int width)
           glVertex2f(x1, y2);
           glVertex2f(x2, y2);
           glEnd();
+
+          x1 += xsquaresize;
         }
         x1 = -1 + xborder;
-        y1 = 1 - yborder;
+
+        y1 -= ysquaresize;
+
+        cout << "\n";
       }
-      cout << "\n";
+      break;
     }
-    break;
-  }
-  default:
-    cout << "Goodbye";
+      // this is a test
+
+    case 2: {
+      // Diamond
+
+      for (row = 1; row <= width; row++) {
+        for (c = 0; c < width - row; c++) {
+          x1 += xsquaresize;
+        }
+        for (c = 1; c <= 2 * row - 1; c++) {
+          x2 = x1 + xsquaresize;
+          y2 = y1 - ysquaresize;
+
+          glColor3f(0.90, 0.22, 0);
+
+          glBegin(GL_QUADS);
+          glVertex2f(x2, y1);
+          glVertex2f(x1, y1);
+          glVertex2f(x1, y2);
+          glVertex2f(x2, y2);
+          glEnd();
+
+          x1 += xsquaresize;
+        }
+        x1 = -1 + xborder;
+
+        y1 -= ysquaresize;
+
+        cout << "\n";
+      }
+
+      for (row = width; row > 1; row--) {
+        for (c = 0; c < width - row + 1; c++) {
+          x1 += xsquaresize;
+        }
+        for (c = 1; c <= 2 * row - 3; c++) {
+          x2 = x1 + xsquaresize;
+          y2 = y1 - ysquaresize;
+
+          glColor3f(0.90, 0.22, 0);
+
+          glBegin(GL_QUADS);
+          glVertex2f(x2, y1);
+          glVertex2f(x1, y1);
+          glVertex2f(x1, y2);
+          glVertex2f(x2, y2);
+          glEnd();
+
+          x1 += xsquaresize;
+        }
+        x1 = -1 + xborder;
+
+        y1 -= ysquaresize;
+      }
+      break;
+    }
+
+    case 3: {
+      int n = 2 * width + 1;
+
+      int i, j, x, y;
+
+      for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+          x = i - width;
+          y = j - width;
+
+          if (x * x + y * y <= width * width + 1) {
+            x1 += i * xsquaresize;
+            y1 -= j * ysquaresize;
+
+            x2 = x1 + xsquaresize;
+            y2 = y1 - ysquaresize;
+
+            glColor3f(0.90, 0.22, 0);
+
+            glBegin(GL_QUADS);
+            glVertex2f(x2, y1);
+            glVertex2f(x1, y1);
+            glVertex2f(x1, y2);
+            glVertex2f(x2, y2);
+            glEnd();
+          }
+          x1 = -1 + xborder;
+          y1 = 1 - yborder;
+        }
+        cout << "\n";
+      }
+      break;
+    }
+    default:
+      cout << "Goodbye";
   }
 }
 
-void init()
-{
+void init() {
   // set clear color to black
   glClearColor(0.0, 0.0, 0.0, 0.0);
 
@@ -198,184 +175,176 @@ void init()
   glColor3f(1.0, 1.0, 1.0);
 }
 
-void ProcessNormalKeys(unsigned char key, int x, int y)
-{
-
+void ProcessNormalKeys(unsigned char key, int x, int y) {
   int c;
 
-  if (key == ' ')
-  {
+  if (key == ' ') {
     cout << "SPACEBAR" << endl;
-    //g->choosing = 1;
+    g->choosing = 1;
     placement_bloc(current_b);
-    for (c = 0; c < g->larg; c++)
-    {
-      if (column_full(c))
-      {
+    for (c = 0; c < g->larg; c++) {
+      if (column_full(c)) {
         delete_col(c);
       }
     }
     free(current_b);
-    current_b = (bloc *)malloc(sizeof(*current_b));
+    current_b = (bloc*)malloc(sizeof(*current_b));
     bloc20(current_b);
-  }
-    glutPostRedisplay();
-
-}
-
-void keyboardown(int key, int x, int y)
-{
-
-  switch (key)
-  {
-  case GLUT_KEY_RIGHT:
-    if (can_right(current_b, g) == 1)
-    {
-      right(current_b);
-    }
-    break;
-
-  case GLUT_KEY_LEFT:
-    if (can_left(current_b, g) == 1)
-    {
-      left(current_b);
-    }
-    break;
-
-  case GLUT_KEY_UP:
-    if (can_top(current_b, g) == 1)
-    {
-      top(current_b);
-    }
-    break;
-
-  case GLUT_KEY_DOWN:
-    if (can_bot(current_b, g) == 1)
-    {
-      bot(current_b);
-    }
-    break;
-
-  case GLUT_KEY_END:
-
-    choosingpiece *= -1;
-
-    break;
-
-  default:
-    break;
   }
   glutPostRedisplay();
 }
 
-void display_bloc(bloc *b, float scaling, float r, float g, float blue, float x, float y)
-{
+void keyboardown(int key, int x, int y) {
+  switch (key) {
+    case GLUT_KEY_RIGHT:
+      if (can_right(current_b, g) == 1) {
+        right(current_b);
+      }
+      break;
 
+    case GLUT_KEY_LEFT:
+      if (can_left(current_b, g) == 1) {
+        left(current_b);
+      }
+      break;
+
+    case GLUT_KEY_UP:
+      if (can_top(current_b, g) == 1) {
+        top(current_b);
+      }
+      break;
+
+    case GLUT_KEY_DOWN:
+      if (can_bot(current_b, g) == 1) {
+        bot(current_b);
+      }
+      break;
+
+    case GLUT_KEY_END:
+
+      choosingpiece *= -1;
+
+      break;
+
+    default:
+      break;
+  }
+  glutPostRedisplay();
+}
+
+void display_bloc(bloc* b,
+                  float scaling,
+                  float r,
+                  float g,
+                  float blue,
+                  float x,
+                  float y) {
   int i, j;
 
   glColor3f(r, g, blue);
 
-  for (i = 0; i < b->haut; i++)
-  {
-    for (j = 0; j < b->larg; j++)
-    {
-      if (b->mat[i][j] == 1)
-      {
+  for (i = 0; i < b->haut; i++) {
+    for (j = 0; j < b->larg; j++) {
+      if (b->mat[i][j] == 1) {
         glBegin(GL_QUADS);
-        glVertex2f(x + j * squaresize * scaling, y - i * squaresize * scaling);
-        glVertex2f(x + (j + 1) * squaresize * scaling, y - i * squaresize * scaling);
-        glVertex2f(x + (j + 1) * squaresize * scaling, y - (i + 1) * squaresize * scaling);
-        glVertex2f(x + (j)*squaresize * scaling, y - (i + 1) * squaresize * scaling);
+        glVertex2f(x + j * xsquaresize * scaling, y - i * ysquaresize * scaling);
+        glVertex2f(x + (j + 1) * xsquaresize * scaling,
+                   y - i * ysquaresize * scaling);
+        glVertex2f(x + (j + 1) * xsquaresize * scaling,
+                   y - (i + 1) * ysquaresize * scaling);
+        glVertex2f(x + (j)*xsquaresize * scaling,
+                   y - (i + 1) * ysquaresize * scaling);
         glEnd();
       }
     }
   }
 }
 
-void dis(game *g, float scaling, float r, float green, float blue, float x, float y)
-{
-
+void dis(game* g,
+         float scaling,
+         float r,
+         float green,
+         float blue,
+         float x,
+         float y) {
   int i, j;
 
-  for (i = 0; i < g->haut; i++)
-  {
-    for (j = 0; j < g->larg; j++)
-    {
-      if (g->board[i][j] == 0)
-      {
+  for (i = 0; i < g->haut; i++) {
+    for (j = 0; j < g->larg; j++) {
+      if (g->board[i][j] == 0) {
         glColor3f(0.2, 0.2, 0.2);
-      }
-      else if (g->board[i][j] == 1)
-      {
+      } else if (g->board[i][j] == 1) {
         glColor3f(0.90, 0.22, 0);
-      }
-      else
-      {
+      } else {
         glColor3f(0.5, 0.5, 0);
       }
       glBegin(GL_QUADS);
-      glVertex2f(x + j * squaresize * scaling, y - i * squaresize * scaling);
-      glVertex2f(x + (j + 1) * squaresize * scaling, y - i * squaresize * scaling);
-      glVertex2f(x + (j + 1) * squaresize * scaling, y - (i + 1) * squaresize * scaling);
-      glVertex2f(x + (j)*squaresize * scaling, y - (i + 1) * squaresize * scaling);
+      glVertex2f(x + j * xsquaresize * scaling, y - i * ysquaresize * scaling);
+      glVertex2f(x + (j + 1) * xsquaresize * scaling,
+                 y - i * ysquaresize * scaling);
+      glVertex2f(x + (j + 1) * xsquaresize * scaling,
+                 y - (i + 1) * ysquaresize * scaling);
+      glVertex2f(x + (j)*xsquaresize * scaling,
+                 y - (i + 1) * ysquaresize * scaling);
       glEnd();
     }
   }
 }
 
-void display()
-{
-
+void display() {
   glClear(GL_COLOR_BUFFER_BIT);
 
   dis(g, 1, 1, 1, 1, x0board, y0board);
-
-  display_bloc(current_b, 1, 1, 1, 1, x0board + current_b->x * squaresize, y0board - current_b->y * squaresize);
-
+  if (g->choosing == 0) {
+    display_bloc(current_b, 1, 1, 1, 1, x0board + current_b->x * xsquaresize,
+                 y0board - current_b->y * ysquaresize);
+  }
   glFlush();
 }
 
-int main(int argc, char **argv)
-{
-
+int main(int argc, char** argv) {
   int i;
 
-  cout << "Triangle: 1   -   Diamond: 2   -   Circle: 3\n"
-       << endl;
+  int windowx = 1000;
+  int windowy = 700;
+
+  cout << "Triangle: 1   -   Diamond: 2   -   Circle: 3\n" << endl;
   cin >> shape;
   cout << "Width" << endl;
   cin >> width;
 
-  if (shape == 1)
-  {
-    squaresize = (2 - 2 * xborder) / (2 * width - 1);
+  if (shape == 1) {
+    xsquaresize = (2 - 2 * xborder) / (2 * width - 1) * windowy/windowx ;
+    ysquaresize = (2 - 2 * yborder) / (2 * width - 1);
   }
-  if (shape == 2)
-  {
-    squaresize = (2 - 2 * xborder) / (2 * width - 1);
-  }
-
-  if (shape == 3)
-  {
-    squaresize = (2 - 2 * xborder) / (2 * width - 1);
+  if (shape == 2) {
+    xsquaresize = (2 - 2 * xborder) / (2 * width - 1)* windowy/windowx;
+    ysquaresize = (2 - 2 * yborder) / (2 * width - 1);
   }
 
-  move_unit = squaresize;
+  if (shape == 3) {
+    xsquaresize = (2 - 2 * xborder) / (2 * width - 1)* windowy/windowx;
+    ysquaresize = (2 - 2 * yborder) / (2 * width - 1);
+  }
+
+  xmove_unit = xsquaresize;
+  ymove_unit = ysquaresize;
+
   cout << "AZE" << endl;
   create_board(shape, width);
   cout << "AZER" << endl;
-  current_b = (bloc *)malloc(sizeof(*current_b));
+  current_b = (bloc*)malloc(sizeof(*current_b));
   bloc7(current_b);
   cout << "AZERT" << endl;
 
   /*
-  create_board(3);
-  blocT5( );
-  add_bloc();
-  display_board();
-  cout << endl << endl;
-  display_bloc();
-  */
+create_board(3);
+blocT5( );
+add_bloc();
+display_board();
+cout << endl << endl;
+display_bloc();
+*/
   //  test_init_blocs();
 
   cout << x0board << endl;
@@ -384,16 +353,16 @@ int main(int argc, char **argv)
   cout << current_b->y << endl;
 
   /*
-  bestiaire(57,57);
+bestiaire(57,57);
 
-  for(i=0 ; i<10 ; i++){
-    cout << rand_a_b(0,100);
-  }
-  */
+for(i=0 ; i<10 ; i++){
+cout << rand_a_b(0,100);
+}
+*/
 
   glutInit(&argc, argv);
   glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-  glutInitWindowSize(1000, 700);
+  glutInitWindowSize(windowx, windowy);
   glutInitWindowPosition(100, 100);
   glutCreateWindow("TETRIS");
   // glutTimerFunc(1.0, animate, 0);
